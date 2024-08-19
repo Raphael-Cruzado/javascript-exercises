9dedcff50bb64124159d6dea6f294852713374c9
Lance Cruzado <raphaellcruzado@gmail.com>
2024-08-19 16:57:41 -0700

    finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeItems) {
+    let newArray = array.filter(inc => !removeItems.includes(inc));
+
+    return newArray;
 };
 
 // Do not edit below this line
