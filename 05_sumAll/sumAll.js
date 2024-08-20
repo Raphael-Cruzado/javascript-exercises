fb496e92bc92ee1f6a4c50d827fc0c0e7edcae84
Lance Cruzado <raphaellcruzado@gmail.com>
2024-08-20 15:45:01 -0700

    working on 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let additive, result = 0;
 
+    if (start > end) {
+        additive = end;
+        result = end;
+    } else {
+        additive = start;
+        result = start;
+    }
+    
+
+    for (let i = start; i < end; i++) {
+        additive += 1;
+        result += additive;
+    }
+
+    return result;
 };
+
+console.log(sumAll(123, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
